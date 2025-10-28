5f3c06da409b13420113731d06e44d96d15fccd7
sbermudo777-dotcom <samuelbermudo777@gmail.com>
2025-10-28 09:58:03 +0100

    Update HiloParImpar.java

--- UT2_Programacion_Multihilo/UT2_01_Hilos1/HiloParImpar.java
+++ UT2_Programacion_Multihilo/UT2_01_Hilos1/HiloParImpar.java
@@ -2,27 +2,41 @@ package ejercicios;
 
 public class HiloParImpar implements Runnable {
 
+	private int tipo;
+
+	public HiloParImpar(int tipo) {
+		super();
+		this.tipo = tipo;
+	}
+
 	public void run() {
-		if (Thread.currentThread().getName() == "Tipo1") { // Tipo 1
+		if (tipo == 1) { // Tipo 1
 			for (int i = 1; i <= 100; i++) {
 				if (i % 2 == 0)
-					System.out.println(" HILO 2 generando número par " + i);
+					System.out.println(" HILO " + tipo + " generando número par " + i);
 			}
 		}
-		if (Thread.currentThread().getName() == "Tipo2") { // Tipo 2
+		if (tipo == 2) { // Tipo 2
 			for (int i = 101; i <= 200; i++) {
 				if (i % 2 != 0)
-					System.out.println(" HILO 2 generando número impar " + i);
+					System.out.println(" HILO " + tipo + " generando número impar " + i);
 			}
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
-		new Thread(new HiloParImpar(), "Tipo1").start();
-		new Thread(new HiloParImpar(), "Tipo2").start();
+		Thread t1 = new Thread(new HiloParImpar(1));
+		Thread t2 = new Thread(new HiloParImpar(2));
 
 		// Modificar el programa para que muestre siempre los impares en primer lugar
+		t2.start(); // Inicia t2 impares
+		t2.join(); // Sigue hasta terminar
+
+		t1.start(); // Inicia t1 pares
+		t1.join(); // Sigue hasta teminar
+
+		System.out.println("Fin del programa."); // Fin
 
 	}
 
