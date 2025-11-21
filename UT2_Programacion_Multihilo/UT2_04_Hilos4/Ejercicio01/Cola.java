a9ebd93b518fa6b01987d7bfbe16f28983514287
sbermudo777-dotcom <samuelbermudo777@gmail.com>
2025-11-21 13:23:44 +0100

    Add synchronized methods for producer and consumer
    Implement synchronization for producer and consumer methods to handle concurrent access issues.

---
+++ UT2_Programacion_Multihilo/UT2_04_Hilos4/Ejercicio01/Cola.java
@@ -0,0 +1,48 @@
+package ut2_03_Hilos4;
+
+import java.util.LinkedList;
+
+public class Cola {
+
+	private LinkedList<Integer> cantidad = new LinkedList<>();
+	private final int MAXIMO = 5; // Máximo 5 elementos producidos en la cola
+
+	/*
+	 * Implementar sincronización en los métodos de cada clase para asegurar que no
+	 * haya problemas de acceso concurrente.
+	 */
+
+	// Sincronización del productor
+	public synchronized void producir(int numero) { // Put
+		while (cantidad.size() == MAXIMO) { // si la cola está llena, espera.
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+
+		cantidad.add(numero);
+		System.out.println("Productor produce: " + numero + " → Cola: " + cantidad.size() + "/5");
+
+		notifyAll(); // avisar a los consumidores que hay dato
+	}
+
+	// Sincronización del consumidor
+	public synchronized int consumir() { // Get
+		while (cantidad.isEmpty()) { // cola vacía → esperar
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+
+		int numero = cantidad.removeFirst();
+		System.out.println("    Consumidor consume: " + numero + " → Cola: " + cantidad.size() + "/5");
+
+		notifyAll(); // avisar al productor que hay hueco
+		return numero;
+	}
+
+}
