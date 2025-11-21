a8a77d53274e51ea507b420fb779ddcf8044163b
sbermudo777-dotcom <samuelbermudo777@gmail.com>
2025-11-21 13:22:31 +0100

    Update entry and exit messages for clients

--- UT2_Programacion_Multihilo /UT2_03_Hilos3/Cola.java
+++ UT2_Programacion_Multihilo /UT2_03_Hilos3/Cola.java
@@ -25,13 +25,13 @@ public class Cola {
 		}
 		// Una vez hay espacio disponible entra un nuevo cliente
 		clientesDentro++;
-		System.out.println("→ Cliente " + clienteId + " ENTRA al gimnasio. (Dentro: " + clientesDentro + ")");
+		System.out.println("   > Cliente " + clienteId + " ENTRA al gimnasio. (Dentro: " + clientesDentro + ")");
 
 	}
 
 	// Método que simula la salida del gimnasio
 	public synchronized void put(int clienteId) { // Salir
-		System.out.println("← Cliente " + clienteId + " SALE del gimnasio. (Dentro: " + (clientesDentro - 1) + ")");
+		System.out.println("< Cliente " + clienteId + " SALE del gimnasio. (Dentro: " + (clientesDentro - 1) + ")");
 		clientesDentro--; // Reducimos menos un cliente para comprobar la función
 		// Despertamos a todos los que estaban esperando (solo uno entrará)
 		notifyAll();
